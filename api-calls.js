53e827f2f9db7cc9b40582176d3ef88d41e1b2d7
Noah Gibson <noah.gibson99@gmail.com>
2020-02-14 19:06:41 -0700

    Add api call to find location

--- api-calls.js
+++ api-calls.js
@@ -1,4 +1,4 @@
-export const getCoordinates = async () => {
+export const getTrails = async () => {
   try {
     const response = await fetch(`${HIKING_PROJECT_ALL_TRAILS_URL}lat=40.0274&lon=-105.2519&maxDistance=10&key=${HIKING_PROJECT_KEY}`)
     if ( !response.ok ) {
@@ -7,5 +7,17 @@ export const getCoordinates = async () => {
     return response.json();
   } catch(error) {
     throw new Error(error);
+  }
+}
+
+export const getCoordinates = async () => {
+  try {
+    const response = await fetch('');
+    if( !response.ok ) {
+      throw new Error('There was an error getting your coordinates')
+    }
+    return response.json();
+  } catch (error) {
+    throw new Error(error)
   }
 }
