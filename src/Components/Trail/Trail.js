1f9bd0d5d386fc8b753af2c17579d3fe9ebee57f
Noah Gibson <noah.gibson99@gmail.com>
2020-02-28 20:21:49 -0700

    Add images to trail component

--- src/Components/Trail/Trail.js
+++ src/Components/Trail/Trail.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Trail = (props) => {
+  console.log(props)
   return (
     <div>
       <h2>{props.trail.name}</h2>
+      <img src={props.trail.imgSmallMed} />
     </div>
   )
 }
