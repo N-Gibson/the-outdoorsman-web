73c6c88349eb9013307cbc28fa8f89e16ae83440
Noah Gibson <noah.gibson99@gmail.com>
2020-03-16 18:46:29 -0600

    Add class boilerplate

---
+++ src/Components/Campgrounds/Campgrounds.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Campgrounds extends Component {
+  constructor() {
+    super();
+    this.state ={
+
+    }
+  }
+
+  componentDidMount() {
+    
+  }
+}
