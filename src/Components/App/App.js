c20d74af5e9733a49177459c5c85e442d29730e5
Noah Gibson <noah.gibson99@gmail.com>
2020-03-16 19:25:30 -0600

    Add routes in app to display campgroud component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Trails from '../Trails/Trails';
 import Campgrounds from '../Campgrounds/Campgrounds';
+import { Route } from 'react-router-dom';
+import Nav from '../Nav/Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Trails />
-      <Campgrounds />
+      <Nav />
+      <Route exact path='/trails' render={() =>  <Trails />}/>
+      <Route exact path='/campgrounds' render={() => <Campgrounds />}/>
     </div>
   );
 }
